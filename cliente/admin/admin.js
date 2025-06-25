70b6569eafe0669f64a98a892d8f543acd9ccc4d
Ederson Torresini <etorresini@ifsc.edu.br>
2025-06-25 00:26:03 -0300

    Limpar jogadores da sala

---
+++ cliente/admin/admin.js
@@ -0,0 +1,27 @@
+/*global io, mqtt*/
+/*eslint no-undef: "error"*/
+const socket = io();
+
+document.getElementById("limpar-sala").addEventListener("click", () => {
+  socket.emit("limpar-sala", "1");
+});
+
+const mqttBrokerUrl = "wss://feira-de-jogos.dev.br/mqtt";
+const client = mqtt.connect(mqttBrokerUrl);
+const logger = document.getElementById("mqtt-logger");
+
+client.on("connect", () => {
+  logger.innerHTML += "<p>Connected to MQTT broker</p>";
+
+  client.subscribe("#", (err) => {
+    if (!err) {
+      logger.innerHTML += "<p>Subscribed to all topics</p>";
+    } else {
+      logger.innerHTML += `<p>Error subscribing: ${err}</p>`;
+    }
+
+    client.on("message", (topic, message) => {
+      logger.innerHTML += `<p><strong>Topic:</strong> ${topic} <strong>Message:</strong> ${message.toString()}</p>`;
+    });
+  });
+});
